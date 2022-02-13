d9c8805903c6f605adb587e56dc88a08e9dca425
himanshu <himanshu.dulani28@gmail.com>
2022-02-13 18:41:14 +0530

    first commit

---
+++ src/components/Content.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Content() {
+    return (
+            <div className='container-fluid bg-success'>
+                <img src="https://images.hungama.com/c/1/467/32a/16950200/16950200_300x300.jpg" class="rounded float-start" alt="..." />
+                <img src="https://im.rediff.com/300-300/movies/2013/sep/25lata-mangeshkar.jpg" class="rounded float-end" alt="..."></img>
+                <img src="https://s.saregama.tech/image/c/m/f/14/94/lata-mangeshkar_1624601998.jpg" class="rounded mx-auto d-block" alt="..."></img>
+                <h1> Legendary-Lata Mangeshkar</h1>
+                <ul>
+                    <li><h3>Born :28 September 1929, Indore</h3></li>
+                    <li><h3>the 'Nightingale of India</h3></li>
+                    <li><h3>Bharat Ratna Awarded</h3></li>
+                    <li><h3>Lata’s patriotic song Ae Mere Watan Ke Logon moved Prime Minister Jawaharlal Nehru to tears</h3></li>
+                    <li><h3>Lata Mangeshkar served as a Member of Parliament in Rajya Sabha</h3></li>
+                    <li><h3>Lata’s fame extends way beyond Indian boundaries</h3></li>
+                    <li><h3>Lata Mangeshkar once held a Guinness world record</h3></li>
+                </ul>
+            </div>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-dark bg-dark justify-content-center">
+                <h1 className='navbar-text'>The Music Sensation</h1>
+            </nav>
+        </div>
+    )
+}
